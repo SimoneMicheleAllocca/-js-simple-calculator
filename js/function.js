013e602b8b62ebb97faddf7c26bc830c3208206f
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-04-15 22:34:09 +0200

    add: first commit

---
+++ js/function.js
@@ -0,0 +1,26 @@
+
+function btnNumeri() {
+    if (displayIn.innerHTML.length <=10) {
+        displayIn.innweHTML+= this.innerHTML;
+    }
+}
+
+
+function operatori () {
+    primoValore = parseInt(displayIn.innerHTML);
+    displayIn.innerHTML = "";
+    console.log(primoValore);
+    operatoreValore = this.innerHTML;
+}
+
+function BtnTotale() {
+    secondoValore = parseInt(displayIn.innerHTML);
+    console.log(secondoValore);
+    switch(operatoreValore) {
+        case "+" :risultato = primoValore + secondoValore ; break;
+        case "-" :risultato = primoValore - secondoValore; break;
+        case "*" :risultato = primoValore * secondoValore; break;
+        case "/" :risultato = primoValore / secondoValore; break;
+    }
+    displayIn.innerHTML = risultato;
+}
---
+++ js/script.js
@@ -0,0 +1,24 @@
+const btnTotal = document.querySelector(".equals");
+const resetBtn = document.querySelector(".c-btn");
+const displayIn = document.querySelector(".result");
+let buttons = document.querySelectorAll(".button-numbers");
+let operators = document.querySelectorAll(".operator");
+let primoValore = 0;
+let secondoValore = 0;
+let operatoreValore = "";
+
+
+for(let i = 0; i < buttons.length; i++) {
+    const valoreBottoni = buttons [i];
+    valoreBottoni.addEventlistener ("click", btnNumeri);
+}
+
+for (let i = 0; i< operators.length; i++) {
+    const valoriOperatori = operators[i];
+    valoriOperatori.addEventListener("click", operators);
+}
+
+btnTotal.addEventListener("click", btnTotal);
+resetBtn.addEventListener("click", function(){
+    window.location.reload();
+})
